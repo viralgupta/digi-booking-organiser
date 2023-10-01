115dae97bad26d96e02de85c094311de7ccabec7
viralgupta <guptaviral0@gmail.com>
2023-10-02 00:48:26 +0530

    testing

--- next.config.js
+++ next.config.js
@@ -3,27 +3,27 @@ module.exports = () => {
     return [
       {
         source: "/api/event/myevents",
-        destination: "https://f85e-103-4-221-252.ngrok-free.app/api/event/myevents",
+        destination: "https://e87d-103-4-221-252.ngrok-free.app/api/event/myevents",
       },
       {
         source: "/api/event/createevent",
-        destination: "https://f85e-103-4-221-252.ngrok-free.app/api/event/createevent",
+        destination: "https://e87d-103-4-221-252.ngrok-free.app/api/event/createevent",
       },
       {
         source: "/api/user/login",
-        destination: "https://f85e-103-4-221-252.ngrok-free.app/api/user/login",
+        destination: "https://e87d-103-4-221-252.ngrok-free.app/api/user/login",
       },
       {
         source: "/api/user/signup",
-        destination: "https://f85e-103-4-221-252.ngrok-free.app/api/user/signup",
+        destination: "https://e87d-103-4-221-252.ngrok-free.app/api/user/signup",
       },
       {
         source: "/api/event/verifyevent",
-        destination: "https://f85e-103-4-221-252.ngrok-free.app/api/event/verifyevent",
+        destination: "https://e87d-103-4-221-252.ngrok-free.app/api/event/verifyevent",
       },
       {
         source: "/api/event/verifybooking",
-        destination: "https://f85e-103-4-221-252.ngrok-free.app/api/event/verifybooking",
+        destination: "https://e87d-103-4-221-252.ngrok-free.app/api/event/verifybooking",
       },
     ];
   };
